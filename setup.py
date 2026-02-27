b23bcfeff0f9b7bbe5ca5d6c7953d9916efbb552
ArunJallawar <arunjallawar888@gmail.com>
2026-02-27 09:07:29 +0000

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Arun Jallawar",
+    author_email="arunjallawar888@gmail.com",
+    packages=find_packages()
+)
